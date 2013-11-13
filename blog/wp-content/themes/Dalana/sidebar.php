ba7896dc087e3c5cd8816f18699b96e79dc36b79
eije <elacuu@gmail.com>
2013-11-13 17:07:12 +0100

    kategori
    inte ser damp ut nu

--- blog/wp-content/themes/Dalana/sidebar.php
+++ blog/wp-content/themes/Dalana/sidebar.php
@@ -9,9 +9,3 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-
-	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-		<div id="secondary" class="widget-area" role="complementary">
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		</div><!-- #secondary -->
-	<?php endif; ?>
