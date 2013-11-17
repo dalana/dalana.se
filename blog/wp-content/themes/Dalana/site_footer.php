98a894384f258ece06e95f257062057203e373a3
eije <elacuu@gmail.com>
2013-11-17 01:49:28 +0100

    footer blog

--- blog/wp-content/themes/Dalana/site_footer.php
+++ blog/wp-content/themes/Dalana/site_footer.php
@@ -1,10 +1,13 @@
     </div> <!-- end of main -->
 <div style="height: 10px"></div>
 </div> <!-- end of wrap -->
-
+		
 <div id="footer">
+    <div id="footer-wrap">
     	<span><a rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/se/"><img alt="Creative Commons-licens" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/2.5/se/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">dalana.se</span> är licensierad under en <a rel="license" href="http://creativecommons.org/licenses/by-sa/2.5/se/">Creative Commons Erkännande-DelaLika 2.5 Sverige licens</a>.</span>
+    </div>
 </div>
+
 
 </body>
 </html>
